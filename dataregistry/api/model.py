1ed31303a169f6603329a30c1473e04a42ebbf7f
Drew Hite <drew@drewhite.org>
2023-02-02 15:41:03 -0500

    fix GET, output metadata properly

--- dataregistry/api/model.py
+++ dataregistry/api/model.py
@@ -1,6 +1,15 @@
 from pydantic import BaseModel
+from sqlalchemy import Column, Integer, JSON
+from sqlalchemy.orm import declarative_base
 
+Base = declarative_base()
 
 class RecordRequest(BaseModel):
     name: str
     metadata: dict
+
+class RecordJson(Base):
+    __tablename__ = "records"
+    id = Column(Integer, primary_key=True)
+    record_metadata = Column(JSON)
+
