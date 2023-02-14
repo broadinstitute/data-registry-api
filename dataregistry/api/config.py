7c7f2a621f2503ce5451d36cead4e60d5c319773
Drew Hite <drew@drewhite.org>
2023-02-14 12:18:26 -0500

    python 3.8 cache

--- dataregistry/api/config.py
+++ dataregistry/api/config.py
@@ -1,10 +1,10 @@
 import json
-from functools import cache
+from functools import lru_cache
 
 from boto3 import Session
 
 
-@cache
+@lru_cache
 def get_sensitive_config():
     client = Session().client('secretsmanager', region_name='us-east-1')
     return json.loads(client.get_secret_value(SecretId='data-registry')['SecretString'])
