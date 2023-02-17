c2362d2cac0a1b709fb537fb4741dc3c01d9ce0d
Drew Hite <drew@drewhite.org>
2023-02-17 12:17:06 -0500

    move sql alchemy to latest version

--- dataregistry/api/query.py
+++ dataregistry/api/query.py
@@ -2,23 +2,23 @@ import datetime
 import json
 import re
 
-from sqlalchemy.orm import Session
+from sqlalchemy import text
 
 from dataregistry.api import s3
 from dataregistry.api.model import Record, SavedRecord
 
 
 def get_all_records(engine) -> list:
-    results = engine.execute(
-        """
-        SELECT s3_bucket_id, name, metadata, data_source_type, data_source, data_type, genome_build,
-            ancestry, data_submitter, data_submitter_email, institution, sex, global_sample_size, t1d_sample_size, 
-            bmi_adj_sample_size, status, additional_data, deleted_at_unix_time as deleted_at, id, created_at 
-            FROM records WHERE deleted_at_unix_time = 0
-        """
-    ).fetchall()
-
-    return [SavedRecord(**fix_json(dict(result))) for result in results]
+    with engine.connect() as conn:
+        results = conn.execute(text(
+            """
+            SELECT s3_bucket_id, name, metadata, data_source_type, data_source, data_type, genome_build,
+                ancestry, data_submitter, data_submitter_email, institution, sex, global_sample_size, t1d_sample_size, 
+                bmi_adj_sample_size, status, additional_data, deleted_at_unix_time as deleted_at, id, created_at 
+                FROM records WHERE deleted_at_unix_time = 0
+            """)
+        )
+    return [SavedRecord(**fix_json(row._asdict())) for row in results]
 
 
 def fix_json(r: dict) -> dict:
@@ -27,23 +27,20 @@ def fix_json(r: dict) -> dict:
 
 
 def get_record(engine, index) -> SavedRecord:
-    session = Session(engine)
-    with session.begin():
-        results = session.execute(
-            """
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("""
             SELECT s3_bucket_id, name, metadata, data_source_type, data_source, data_type, genome_build,
                 ancestry, data_submitter, data_submitter_email, institution, sex, global_sample_size, t1d_sample_size, 
                 bmi_adj_sample_size, status, additional_data, deleted_at_unix_time as deleted_at, id, created_at
                 FROM records r WHERE r.id = :id 
-            """, {'id': index}
-        ).fetchall()
+            """), {'id': index}
+        ).first()
 
-        if len(results) == 0:
+        if result is None:
             raise ValueError(f"No records for id {index}")
-        elif len(results) > 1:
-            raise ValueError(f"{len(results)} records for id {index}, should be unique")
         else:
-            return SavedRecord(**fix_json(dict(results[0])))
+            return SavedRecord(**fix_json(result._asdict()))
 
 
 def convert_name_to_s3_bucket_id(name):
@@ -53,29 +50,29 @@ def convert_name_to_s3_bucket_id(name):
 
 def insert_record(engine, data: Record):
     s3_record_id = convert_name_to_s3_bucket_id(data.name)
-    session = Session(engine)
-    with session.begin():
+    with engine.connect() as conn:
         sql_params = data.dict()
         sql_params.update({'s3_bucket_id': s3_record_id, 'metadata': json.dumps(data.metadata)})
-        session.execute("""
+        conn.execute(text("""
             INSERT INTO records (s3_bucket_id, name, metadata, data_source_type, data_source, data_type, genome_build,
             ancestry, data_submitter, data_submitter_email, institution, sex, global_sample_size, t1d_sample_size, 
             bmi_adj_sample_size, status, additional_data) VALUES(:s3_bucket_id, :name, :metadata, :data_source_type, 
             :data_source, :data_type, :genome_build, :ancestry, :data_submitter, :data_submitter_email, :institution, 
             :sex, :global_sample_size, :t1d_sample_size, :bmi_adj_sample_size, :status, :additional_data)
-        """, sql_params)
+        """), sql_params)
+        conn.commit()
         s3.create_record_directory(s3_record_id)
     return s3_record_id
 
 
 def delete_record(engine, index):
-    session = Session(engine)
-    with session.begin():
+    with engine.connect() as conn:
         s3_record_id = get_record(engine, index).s3_bucket_id
-        engine.execute(
-            """
-            UPDATE records r SET r.deleted_at_unix_time = UNIX_TIMESTAMP() WHERE r.id = {} 
-            """.format(index)
+        conn.execute(
+            text("""
+            UPDATE records r SET r.deleted_at_unix_time = UNIX_TIMESTAMP() WHERE r.id = :id 
+            """), {'id': index}
         )
+        conn.commit()
         s3.delete_record_directory(s3_record_id)
     return s3_record_id
