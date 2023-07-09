e61f46a4030a2357b82da9e01c148f225c53ff0d
Drew Hite <drew@drewhite.org>
2023-07-08 23:04:38 -0400

    cloudfront

--- dataregistry/server.py
+++ dataregistry/server.py
@@ -31,7 +31,8 @@ origins = [
     "http://localhost:3000",
     "http://localhost",
     "http://data-registry-vue.s3-website-us-east-1.amazonaws.com",
-    "https://dcicue2yl1fq.cloudfront.net"
+    "https://dcicue2yl1fq.cloudfront.net",
+    "https://d68r3fsifmwz.cloudfront.net"
 ]
 # enable cross-origin resource sharing
 app.add_middleware(
