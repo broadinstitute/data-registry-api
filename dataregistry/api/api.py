e78d6fe5dab55b4db8b4df76eaba520f17ac50fe
Drew Hite <drew@drewhite.org>
2023-08-17 08:30:20 -0400

    quick fix for cloudfront

--- dataregistry/api/api.py
+++ dataregistry/api/api.py
@@ -342,8 +342,7 @@ def login(response: Response, creds: UserCredentials):
         raise fastapi.HTTPException(status_code=401, detail='Invalid username or password')
     response.set_cookie(key=AUTH_COOKIE_NAME, value=get_encoded_cookie_data(user if user else
                                                                            User(name=creds.email, email=creds.email,
-                                                                                role='user')),
-                        domain='.cloudfront.net', samesite='lax')
+                                                                                role='user')))
     return {'status': 'success'}
 
 
