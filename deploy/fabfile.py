0f6245e24176adab49ef0d6e002ce1c9d3d88a97
Drew Hite <drew@drewhite.org>
2023-09-06 16:37:22 -0400

    fabric file to ease the deploy process

--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -2,17 +2,28 @@ from fabric import task
 
 
 @task
-def deploy(c, env, commit):
-    if env == 'prod':
-        c.run("echo 'Deploying to prod!'")
-        c.run("cd ~/data-registry-api-prd")
-    else:
-        c.run("echo 'Deploying to dev!'")
-        c.run("cd ~/data-registry-api-qa")
-    if commit:
-        c.run(f"git checkout {commit}")
-    else:
-        c.run("git pull")
+def deploy(c, env, commit=None):
+    """
+    Deploy the application to the specified environment.
+
+    Parameters:
+        c: The connection context.
+        env: The environment (e.g., 'dev', 'prd').
+        commit: The commit to deploy. If not specified, the latest commit on main.
+    """
+    update_source(c, env, commit)
+    migrate(c, env)
+    restart(c, env)
+
+
+@task
+def update_source(c, env, commit):
+    directory = get_checkout_directory(env)
+    with c.cd(directory):
+        if commit:
+            c.run(f"git checkout {commit}")
+        else:
+            c.run("git checkout main; git pull")
 
 
 @task
@@ -24,12 +35,29 @@ def restart(c, env):
         c: The connection context.
         env: The environment (e.g., 'dev', 'prd').
     """
-    directory = "/home/ec2-user/data-registry-api-qa" if env == 'dev' else "/home/ec2-user/data-registry-api-prd"
+    directory = get_checkout_directory(env)
     screen_session = "dr-api-dev" if env == 'dev' else "dr-api-prd"
-    port = 8000 if env == 'dev' else 443
+    port = 80 if env == 'dev' else 443
 
     with c.cd(directory):
         # terminate running screen sessions
-        c.run(f"screen -ls | grep -o '[0-9]*\.{screen_session}' | while read -r line; do screen -S \"${{line}}\" -X quit; done")
+        c.run(
+            f"screen -ls | grep -o '[0-9]*\.{screen_session}' | while read -r line; do screen -S \"${{line}}\" -X quit; done")
+        c.run("python3.8 -m pip install -r requirements.txt")
         c.run(
             f"screen -dmS {screen_session} bash -c 'python3.8 -m dataregistry.main -e .env serve --port {port}'")
+
+
+def get_checkout_directory(env):
+    return "/home/ec2-user/data-registry-api-qa" if env == 'dev' else "/home/ec2-user/data-registry-api-prd"
+
+
+@task
+def migrate(c, env):
+    """
+    Run db migrations.
+    """
+    directory = get_checkout_directory(env)
+    with c.cd(directory):
+        db = "dataregistry_qa" if env == 'dev' else "dataregistry"
+        c.run(f"export DATA_REGISTRY_DB_NAME={db}; python3.8 -m alembic upgrade head")
