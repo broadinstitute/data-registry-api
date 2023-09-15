15fabeb4d4a4f40464bccaed48e318560f9a41c5
Drew Hite <drew@drewhite.org>
2023-09-15 14:36:02 -0400

    working out some kinks

--- dataregistry/api/api.py
+++ dataregistry/api/api.py
@@ -145,7 +145,7 @@ async def save_file_for_phenotype(data_set_id: str, phenotype: str, dichotomous:
                                     cases: int = None, controls: int = None):
     try:
         pd_id = query.insert_phenotype_data_set(engine, data_set_id, phenotype,
-                                                f"s3://dig-analysis-data/variants_raw/{file_path}", dichotomous,
+                                                f"s3://dig-analysis-data/{file_path}", dichotomous,
                                                 sample_size, cases, controls, filename, file_size)
         return {"message": f"Successfully saved {filename}", "phenotype_data_set_id": pd_id}
     except Exception as e:
