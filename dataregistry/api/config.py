9a4980a41467d2a1197bae8ec6c16f82738f6018
Drew Hite <drew@drewhite.org>
2025-02-14 12:55:31 -0500

    jenkins integration tests

--- dataregistry/api/config.py
+++ dataregistry/api/config.py
@@ -2,7 +2,6 @@ import json
 from functools import lru_cache
 
 from boto3 import Session
-from botocore.exceptions import NoCredentialsError
 
 
 @lru_cache
@@ -10,5 +9,6 @@ def get_sensitive_config():
     try:
         client = Session().client('secretsmanager', region_name='us-east-1')
         return json.loads(client.get_secret_value(SecretId='data-registry')['SecretString'])
-    except NoCredentialsError as e:
+    except Exception as e:
+        print(f"Failed to get sensitive config: {str(e)}")
         return None
