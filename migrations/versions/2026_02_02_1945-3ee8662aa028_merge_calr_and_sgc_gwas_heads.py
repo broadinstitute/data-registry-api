7e3b7611da4ab83c715d7e219ae055be069d7003
Drew Hite <drew@drewhite.org>
2026-02-02 19:50:32 -0500

    phase 1 refinements for sgc

--- migrations/versions/2026_02_02_1945-3ee8662aa028_merge_calr_and_sgc_gwas_heads.py
+++ migrations/versions/2026_02_02_1945-3ee8662aa028_merge_calr_and_sgc_gwas_heads.py
@@ -1,7 +1,7 @@
 """merge_calr_and_sgc_gwas_heads
 
 Revision ID: 3ee8662aa028
-Revises: add_cases_controls_to_sgc_gwas_files, create_calr_files
+Revises: sgc_gwas_cases_ctrl, create_calr_files
 Create Date: 2026-02-02 19:45:59.909407
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '3ee8662aa028'
-down_revision = ('add_cases_controls_to_sgc_gwas_files', 'create_calr_files')
+down_revision = ('sgc_gwas_cases_ctrl', 'create_calr_files')
 branch_labels = None
 depends_on = None
 
